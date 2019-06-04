c2bd03d112d44a52cfcd5430abbd8301d4cdc707
sim82 <simberger@gmail.com>
2019-06-04 21:36:39 +0200

    wrote semi useful test, just to have one

--- src/node.rs
+++ src/node.rs
@@ -38,13 +38,44 @@ impl<T: Send> CommandNode<T> {
     }
 
     pub fn join(self) {
-        let CommandNode {
-            join_handle,
-            ..
-            // command_sender,
-            // stop_command,
-        } = self;
+        let CommandNode { join_handle, .. } = self;
 
         join_handle.join().unwrap();
+    }
+}
+
+mod tests {
+    use super::CommandNode;
+    use std::sync::mpsc::channel;
+    use std::thread::spawn;
+    use std::time::Duration;
+    use std::time::Instant;
+    enum Command {
+        Stop,
+    }
+    #[test]
+    fn test_node_lifecycle() {
+        let (send, recv) = channel();
+
+        let handle = spawn(move || loop {
+            match recv.recv() {
+                Ok(Command::Stop) => break,
+                _ => (),
+            }
+        });
+        let node = CommandNode::new(handle, send, Command::Stop);
+
+        let sender2 = node.clone_sender();
+        let handle2 = spawn(move || {
+            std::thread::sleep(Duration::from_millis(500));
+            sender2.send(Command::Stop).expect("join failed");
+        });
+
+        let t1 = Instant::now();
+        println!("waiting for node to stop ...");
+        node.join();
+        println!("stopped");
+        assert!(Instant::now().duration_since(t1) > Duration::from_millis(300));
+        handle2.join().expect("join failed");
     }
 }
